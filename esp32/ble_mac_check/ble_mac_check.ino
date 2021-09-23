a1ea673a1209e32517763b196458eb8d6b0de080
Remma Matsuda <titech.renma@gmail.com>
2021-09-23 19:31:24 +0900

    Added BLE mac check code.

---
+++ esp32/ble_mac_check/ble_mac_check.ino
@@ -0,0 +1,13 @@
+uint8_t macBT[6];
+
+void setup(void) {
+  Serial.begin(115200);
+  Serial.println("-----------------");
+  esp_read_mac(macBT, ESP_MAC_BT);
+  Serial.printf("%02X:%02X:%02X:%02X:%02X:%02X\r\n", macBT[0], macBT[1], macBT[2], macBT[3], macBT[4], macBT[5]);
+}
+
+void loop() {
+  delay(1000);
+  Serial.printf("%02X:%02X:%02X:%02X:%02X:%02X\r\n", macBT[0], macBT[1], macBT[2], macBT[3], macBT[4], macBT[5]);
+}
